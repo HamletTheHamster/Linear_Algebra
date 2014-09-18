da22a71ce1f510e3e8d5172295ab771857e7e5da
Joel N. Johnson <joel.johnson675@gmail.com>
2014-09-17 17:55:43 -0700

    Create 2x2

---
+++ det2x2.m
@@ -0,0 +1,11 @@
+function [ detA ] = det2x2( A )
+%This function finds the determinant of a 2x2 matrix.
+%
+%   Input variable must be a 2x2 matrix.
+%
+%   Coded by: Joel N. Johnson
+%---------------------------------------------------------------
+
+detA = (A(1,1) * A(2,2)) - (A(2,1) * A(1,2));
+
+end
